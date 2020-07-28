6ebd749f5e6824533b47271e590accf857e154be
Markus Ressel <mail@markusressel.de>
2020-07-28 16:32:39 +0200

    added os type distinction

--- zfs-inplace-rebalancing.sh
+++ zfs-inplace-rebalancing.sh
@@ -12,11 +12,38 @@ function rebalance () {
 
     echo "${file_path}"
     echo "Copying '${file_path}' to '${file_path}${tmp_extension}'..."
-    # -a -- keep attributes
-    # -d -- keep symlinks (dont copy target)
-    # -x -- stay on one system
-    # -p -- preserve ACLs too
-    cp -adxp "${file_path}" "${file_path}${tmp_extension}"
+
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        # Linux
+
+        # -a -- keep attributes
+        # -d -- keep symlinks (dont copy target)
+        # -x -- stay on one system
+        # -p -- preserve ACLs too
+        cp -adxp "${file_path}" "${file_path}${tmp_extension}"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+
+        # (should be the same as bsd, but untested!)
+
+        # -a -- keep attributes
+        # -d -- keep symlinks (dont copy target)
+        # -x -- stay on one system
+        # -p -- preserve ACLs too
+        cp -adxp "${file_path}" "${file_path}${tmp_extension}"
+    elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # FreeBSD
+
+        # -a -- Archive mode.  Same as -RpP.
+        # -x -- File system mount points are not traversed.
+        # -p -- Cause cp to preserve the following attributes of each source file
+        #       in the copy: modification time, access time, file flags, file mode,
+        #       ACL, user ID, and group ID, as allowed by permissions.
+        cp -axp "${file_path}" "${file_path}${tmp_extension}"
+    else
+        echo "Unsupported OS type: $OSTYPE"
+        exit 1
+    fi
 
     echo "Removing original '${file_path}'..."
     rm "${file_path}"
