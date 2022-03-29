ae8747c6d6422faded21ae4684db899c0c2b6345
Markus Ressel <mail@markusressel.de>
2022-03-29 03:48:28 +0200

    run multiple variants of the command

--- testing.sh
+++ testing.sh
@@ -8,9 +8,17 @@ set -u
 test_data_src=./test/pool
 test_pool_data_path=./testing_data
 
-rm -f rebalance_db.txt
-rm -rf $test_pool_data_path
+function prepare() {
+    rm -f rebalance_db.txt
+    rm -rf $test_pool_data_path
+    cp -rf $test_data_src $test_pool_data_path
+}
 
-cp -rf $test_data_src $test_pool_data_path
+prepare
+./zfs-inplace-rebalancing.sh $test_pool_data_path
 
+prepare
 ./zfs-inplace-rebalancing.sh --checksum true --passes 1 $test_pool_data_path
+
+prepare
+./zfs-inplace-rebalancing.sh --checksum false $test_pool_data_path
