ff06207df273fb1bd74fa84b51e5b26034985397
Markus Ressel <mail@markusressel.de>
2022-03-29 03:59:01 +0200

    added assertions

--- testing.sh
+++ testing.sh
@@ -5,20 +5,38 @@ set -e
 # exit on undeclared variable
 set -u
 
+log_std_file=./test.log
+log_error_file=./error.log
 test_data_src=./test/pool
 test_pool_data_path=./testing_data
 
 function prepare() {
-    rm -f rebalance_db.txt
-    rm -rf $test_pool_data_path
-    cp -rf $test_data_src $test_pool_data_path
+  # cleanup
+  rm -f $log_std_file
+  rm -f $log_error_file
+  rm -f rebalance_db.txt
+  rm -rf $test_pool_data_path
+
+  # setup
+  cp -rf $test_data_src $test_pool_data_path
+}
+
+function assertions() {
+  # check error log is empty
+  if grep -q '[^[:space:]]' $log_error_file; then
+    echo "error log is not empty!"
+    exit 1
+  fi
 }
 
 prepare
-./zfs-inplace-rebalancing.sh $test_pool_data_path
+./zfs-inplace-rebalancing.sh $test_pool_data_path >> $log_std_file 2>> $log_error_file
+assertions
 
 prepare
-./zfs-inplace-rebalancing.sh --checksum true --passes 1 $test_pool_data_path
+./zfs-inplace-rebalancing.sh --checksum true --passes 1 $test_pool_data_path >> $log_std_file 2>> $log_error_file
+assertions
 
 prepare
-./zfs-inplace-rebalancing.sh --checksum false $test_pool_data_path
+./zfs-inplace-rebalancing.sh --checksum false $test_pool_data_path >> $log_std_file 2>> $log_error_file
+assertions
