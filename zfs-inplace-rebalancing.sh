eaa969e1db32a384f77f3bf6e8392cd3944394ac
meo <meo.prn@outlook.fr>
2024-12-14 10:27:02 +0100

    fix: macOS
    remove all the lower-case casting

--- zfs-inplace-rebalancing.sh
+++ zfs-inplace-rebalancing.sh
@@ -59,7 +59,7 @@ function rebalance() {
     # this shouldn't be needed in the typical case of `find` only finding files with links == 1
     # but this can run for a long time, so it's good to double check if something changed
     if [[ "${skip_hardlinks_flag}" == "true"* ]]; then
-        if [[ "${OSTYPE,,}" == "linux-gnu"* ]]; then
+        if [[ "${OSTYPE}" == "linux-gnu"* ]]; then
             # Linux
             #
             #  -c  --format=FORMAT
@@ -68,7 +68,7 @@ function rebalance() {
             #  %h     number of hard links
 
             hardlink_count=$(stat -c "%h" "${file_path}")
-        elif [[ "${OSTYPE,,}" == "darwin"* ]] || [[ "${OSTYPE,,}" == "freebsd"* ]]; then
+        elif [[ "${OSTYPE}" == "darwin"* ]] || [[ "${OSTYPE}" == "freebsd"* ]]; then
             # Mac OS
             # FreeBSD
             #  -f format
@@ -108,7 +108,7 @@ function rebalance() {
     tmp_file_path="${file_path}${tmp_extension}"
 
     echo "Copying '${file_path}' to '${tmp_file_path}'..."
-    if [[ "${OSTYPE,,}" == "linux-gnu"* ]]; then
+    if [[ "${OSTYPE}" == "linux-gnu"* ]]; then
         # Linux
 
         # --reflink=never -- force standard copy (see ZFS Block Cloning)
@@ -116,7 +116,7 @@ function rebalance() {
         #       -p -- preserve ACLs to
         # -x -- stay on one system
         cp --reflink=never -ax "${file_path}" "${tmp_file_path}"
-    elif [[ "${OSTYPE,,}" == "darwin"* ]] || [[ "${OSTYPE,,}" == "freebsd"* ]]; then
+    elif [[ "${OSTYPE}" == "darwin"* ]] || [[ "${OSTYPE}" == "freebsd"* ]]; then
         # Mac OS
         # FreeBSD
 
@@ -131,9 +131,9 @@ function rebalance() {
     fi
 
     # compare copy against original to make sure nothing went wrong
-    if [[ "${checksum_flag,,}" == "true"* ]]; then
+    if [[ "${checksum_flag}" == "true"* ]]; then
         echo "Comparing copy against original..."
-        if [[ "${OSTYPE,,}" == "linux-gnu"* ]]; then
+        if [[ "${OSTYPE}" == "linux-gnu"* ]]; then
             # Linux
 
             # file attributes
@@ -151,7 +151,7 @@ function rebalance() {
             copy_md5="${copy_md5} $(ls -lha "${tmp_file_path}" | awk '{print $1 " " $3 " " $4}')"
             # file content
             copy_md5="${copy_md5} $(md5sum -b "${tmp_file_path}" | awk '{print $1}')"
-        elif [[ "${OSTYPE,,}" == "darwin"* ]] || [[ "${OSTYPE,,}" == "freebsd"* ]]; then
+        elif [[ "${OSTYPE}" == "darwin"* ]] || [[ "${OSTYPE}" == "freebsd"* ]]; then
             # Mac OS
             # FreeBSD
 
