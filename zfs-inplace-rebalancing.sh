bd8d447150419337bc49f0872038cb0da59c4203
meo <83820503+meo-pill@users.noreply.github.com>
2024-12-17 00:33:35 +0100

    Test mac os (#53)
    * add: test on MacOS
    
    Add test for MacOS in Github action
    
    Maybe see this to add test for FreeBSD :
    https://github.com/marketplace/actions/freebsd-vm
    
    * add: log to test
    
    adding log to the test for better understanding
    
    * fix: shellcheck
    
    remove the yellow color that is unused in the test
    
    * fix: fail on lower-casting
    
    the `"${variable,,}"` seam to not work on macOS so y remove it for the `skip_hardlink_flag` because it's always lowercase
    
    * fix: macOS
    
    remove all the lower-case casting
    
    * fix(perf): going to `stat` remove `lsattr` macOS
    
    The command `lsattr` is not supported on macOS so I have remove it
    switch to `stat` instead of `ls` for better performance and no subshell
    use of parameter expansion with  `%` in place of `awk` for performance and no subshell
    
    * fix: forgot to remove variable

--- zfs-inplace-rebalancing.sh
+++ zfs-inplace-rebalancing.sh
@@ -58,8 +58,8 @@ function rebalance() {
     # check if file has >=2 links in the case of --skip-hardlinks
     # this shouldn't be needed in the typical case of `find` only finding files with links == 1
     # but this can run for a long time, so it's good to double check if something changed
-    if [[ "${skip_hardlinks_flag,,}" == "true"* ]]; then
-        if [[ "${OSTYPE,,}" == "linux-gnu"* ]]; then
+    if [[ "${skip_hardlinks_flag}" == "true"* ]]; then
+        if [[ "${OSTYPE}" == "linux-gnu"* ]]; then
             # Linux
             #
             #  -c  --format=FORMAT
@@ -68,7 +68,7 @@ function rebalance() {
             #  %h     number of hard links
 
             hardlink_count=$(stat -c "%h" "${file_path}")
-        elif [[ "${OSTYPE,,}" == "darwin"* ]] || [[ "${OSTYPE,,}" == "freebsd"* ]]; then
+        elif [[ "${OSTYPE}" == "darwin"* ]] || [[ "${OSTYPE}" == "freebsd"* ]]; then
             # Mac OS
             # FreeBSD
             #  -f format
@@ -108,7 +108,7 @@ function rebalance() {
     tmp_file_path="${file_path}${tmp_extension}"
 
     echo "Copying '${file_path}' to '${tmp_file_path}'..."
-    if [[ "${OSTYPE,,}" == "linux-gnu"* ]]; then
+    if [[ "${OSTYPE}" == "linux-gnu"* ]]; then
         # Linux
 
         # --reflink=never -- force standard copy (see ZFS Block Cloning)
@@ -116,7 +116,7 @@ function rebalance() {
         #       -p -- preserve ACLs to
         # -x -- stay on one system
         cp --reflink=never -ax "${file_path}" "${tmp_file_path}"
-    elif [[ "${OSTYPE,,}" == "darwin"* ]] || [[ "${OSTYPE,,}" == "freebsd"* ]]; then
+    elif [[ "${OSTYPE}" == "darwin"* ]] || [[ "${OSTYPE}" == "freebsd"* ]]; then
         # Mac OS
         # FreeBSD
 
@@ -131,45 +131,48 @@ function rebalance() {
     fi
 
     # compare copy against original to make sure nothing went wrong
-    if [[ "${checksum_flag,,}" == "true"* ]]; then
+    if [[ "${checksum_flag}" == "true"* ]]; then
         echo "Comparing copy against original..."
-        if [[ "${OSTYPE,,}" == "linux-gnu"* ]]; then
+        if [[ "${OSTYPE}" == "linux-gnu"* ]]; then
             # Linux
 
             # file attributes
-            original_md5=$(lsattr "${file_path}" | awk '{print $1}')
-            # file permissions, owner, group
-            # shellcheck disable=SC2012
-            original_md5="${original_md5} $(ls -lha "${file_path}" | awk '{print $1 " " $3 " " $4}')"
+            original_md5=$(lsattr "${file_path}")
+            # remove anything after the last space
+            original_md5=${original_md5% *}
+            # file permissions, owner, group, size, modification time
+            original_md5="${original_md5} $(stat -c "%A %U %G %s %Y" "${file_path}")"
             # file content
-            original_md5="${original_md5} $(md5sum -b "${file_path}" | awk '{print $1}')"
+            original_md5="${original_md5} $(md5sum -b "${file_path}")"
+
 
             # file attributes
-            copy_md5=$(lsattr "${tmp_file_path}" | awk '{print $1}')
-            # file permissions, owner, group
-            # shellcheck disable=SC2012
-            copy_md5="${copy_md5} $(ls -lha "${tmp_file_path}" | awk '{print $1 " " $3 " " $4}')"
+            copy_md5=$(lsattr "${tmp_file_path}")
+            # remove anything after the last space
+            copy_md5=${copy_md5% *}
+            # file permissions, owner, group, size, modification time
+            copy_md5="${copy_md5} $(stat -c "%A %U %G %s %Y" "${tmp_file_path}")"
             # file content
-            copy_md5="${copy_md5} $(md5sum -b "${tmp_file_path}" | awk '{print $1}')"
-        elif [[ "${OSTYPE,,}" == "darwin"* ]] || [[ "${OSTYPE,,}" == "freebsd"* ]]; then
+            copy_md5="${copy_md5} $(md5sum -b "${tmp_file_path}")"
+            # remove the temporary extension
+            copy_md5=${copy_md5%"${tmp_extension}"}
+        elif [[ "${OSTYPE}" == "darwin"* ]] || [[ "${OSTYPE}" == "freebsd"* ]]; then
             # Mac OS
             # FreeBSD
 
-            # file attributes
-            original_md5=$(lsattr "${file_path}" | awk '{print $1}')
-            # file permissions, owner, group
-            # shellcheck disable=SC2012
-            original_md5="${original_md5} $(ls -lha "${file_path}" | awk '{print $1 " " $3 " " $4}')"
+            # note: no lsattr on Mac OS or FreeBSD
+
+            # file permissions, owner, group size, modification time
+            original_md5="$(stat -f "%Sp %Su %Sg %z %m" "${file_path}")"
             # file content
             original_md5="${original_md5} $(md5 -q "${file_path}")"
 
-            # file attributes
-            copy_md5=$(lsattr "${tmp_file_path}" | awk '{print $1}')
-            # file permissions, owner, group
-            # shellcheck disable=SC2012
-            copy_md5="${copy_md5} $(ls -lha "${tmp_file_path}" | awk '{print $1 " " $3 " " $4}')"
+            # file permissions, owner, group size, modification time
+            copy_md5="$(stat -f "%Sp %Su %Sg %z %m" "${tmp_file_path}")"
             # file content
             copy_md5="${copy_md5} $(md5 -q "${tmp_file_path}")"
+            # remove the temporary extension
+            copy_md5=${copy_md5%"${tmp_extension}"}
         else
             echo "Unsupported OS type: $OSTYPE"
             exit 1
@@ -254,7 +257,7 @@ color_echo "$Cyan" "  Use Checksum: ${checksum_flag}"
 color_echo "$Cyan" "  Skip Hardlinks: ${skip_hardlinks_flag}"
 
 # count files
-if [[ "${skip_hardlinks_flag,,}" == "true"* ]]; then
+if [[ "${skip_hardlinks_flag}" == "true"* ]]; then
     file_count=$(find "${root_path}" -type f -links 1 | wc -l)
 else
     file_count=$(find "${root_path}" -type f | wc -l)
@@ -269,7 +272,7 @@ fi
 
 # recursively scan through files and execute "rebalance" procedure
 # in the case of --skip-hardlinks, only find files with links == 1
-if [[ "${skip_hardlinks_flag,,}" == "true"* ]]; then
+if [[ "${skip_hardlinks_flag}" == "true"* ]]; then
     find "$root_path" -type f -links 1 -print0 | while IFS= read -r -d '' file; do rebalance "$file"; done
 else
     find "$root_path" -type f -print0 | while IFS= read -r -d '' file; do rebalance "$file"; done
